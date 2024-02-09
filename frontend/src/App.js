729580ffbe5c85a92a0b6e741bde84cb06600754
shubham_verma <shubham_verma@persistent.com>
2024-02-09 11:07:23 +0530

    appointment table created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import PatientTable from './components/PatientTable';
+import AppointmentTable from './components/AppointmentTable';
 
 function App() {
 
   return (
     <div className="App">
       <Navbar />
-      <PatientTable />
+      {/* <PatientTable /> */}
+      <AppointmentTable />
     </div>
   );
 }
---
+++ frontend/src/components/AppointmentTable.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+export default function AppointmentTable() {
+    const data = [
+        {
+            "appointment_date": "15/02/24",
+            "id": 1,
+            "patient_id": 2,
+            "patient_name": "Krishna Yadav",
+            "slot": 1
+        },
+        {
+            "appointment_date": "20/02/24",
+            "id": 2,
+            "patient_id": 1,
+            "patient_name": "Swati Sharma",
+            "slot": 1
+        }
+    ]
+    return (
+        <div className="mx-5 mt-5">
+            <div className="fs-1 text-center mb-2">Our Patients</div>
+            <table className="table table-striped border ">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th>Patient</th>
+                        <th>Appointment Date</th>
+                        <th>Slot</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((item, index) => {
+                            return <tr key={index}>
+                                <th scope="row">{index + 1}</th>
+                                <td>{item['patient_name']}</td>
+                                <td>{item['appointment_date']}</td>
+                                <td>{item['slot']}</td>
+                                <td>
+                                    <a className="me-3 text-dark" href=""><i class="far fa-edit"></i></a>
+                                    <a className="me-3 text-dark" href=""><i class="far fa-address-book"></i></a>
+                                    <a className="text-danger" href=""><i class="fas fa-trash-alt"></i></a>
+                                </td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
