035833c78908f450f65a476f5b6b72b87f9ac683
shubham_verma <shubham_verma@persistent.com>
2024-02-09 15:51:56 +0530

    setup cors

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:shubham1234@localhost:5432/mini_project_flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+CORS(app)
 
 db = SQLAlchemy(app)
 
